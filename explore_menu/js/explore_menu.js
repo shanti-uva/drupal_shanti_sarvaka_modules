d65a971e41c07ef25e03b59f833c114990795783
mferrara <mf8yk@virginia.edu>
2015-03-31 17:31:58 -0600

    more testing speed on slide toggle

--- explore_menu/js/explore_menu.js
+++ explore_menu/js/explore_menu.js
@@ -3,7 +3,7 @@
     attach: function (context, settings) {
      if(context == document) {
 	     $('.explore>a').bind('click', function(e){
-					$('.collections').slideToggle(150);
+					$('.collections').slideToggle(1500);
 				});
 			}
     }
