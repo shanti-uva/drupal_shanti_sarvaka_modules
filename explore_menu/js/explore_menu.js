5ddf47359ab5cf080e563220b1401b70a2e2731c
mferrara <mf8yk@virginia.edu>
2015-04-11 11:09:19 -0500

    reformat explore collections toggle script

---
+++ explore_menu/js/explore_menu.js
@@ -0,0 +1,16 @@
+(function ($) {
+  Drupal.behaviors.exploreMenu = {
+    attach: function (context, settings) {
+     if(context == document) {
+	     $('.explore > a').click( function(event){
+	           event.stopPropagation();
+	           $('.collections').slideToggle(200);
+	       });
+	       
+	     // $('.collections .close').click( function(event){
+	     //      event.stopPropagation();
+	     //      $('.collections').slideToggle(200);
+	     // });
+    }
+  };
+})(jQuery);
